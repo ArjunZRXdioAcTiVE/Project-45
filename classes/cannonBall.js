909476d62552db7b0223661b6e727cd9e06ea806
ArjunZRXdioAcTiVE <87854934+ArjunZRXdioAcTiVE@users.noreply.github.com>
2022-08-30 08:00:21 +0800

    Add files via upload

---
+++ classes/cannonBall.js
@@ -0,0 +1,32 @@
+class CannonBall {
+    constructor() {
+        this.ball;      
+    }
+
+    createBall () {
+        this.ball = Bodies.circle (
+            player.positionX,
+            cannonLauncher1.position.y,
+            40,
+            {isStatic: true}
+        );
+        World.add(world, this.ball);
+    }
+
+    displayBall () {
+        var ballPos = this.ball.position;
+        circle(ballPos.x, ballPos.y, 40);
+    }
+
+    shoot (launcher) {
+        var newAngle = launcher.rotation - 28;
+        newAngle = newAngle *(3.14/180)
+        var velocity = p5.Vector.fromAngle(newAngle);
+        velocity.mult(0.5);
+        Body.setStatic(this.ball, false);
+        Body.setVelocity(this.ball, {
+            x: velocity.x *(180/3.14), 
+            y: velocity.y * (180/3.14)
+        });
+    }
+}
